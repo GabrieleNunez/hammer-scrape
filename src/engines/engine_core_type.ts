b2473ebde0bbb603f5261905dff2a222d55de504
GabrieleNunez <g_m_nunez@live.com>
2019-07-28 16:16:41 -0700

    Fleshed out engine interfaces a bit more

--- src/engines/engine_core_type.ts
+++ src/engines/engine_core_type.ts
@@ -3,17 +3,30 @@
  */
 export enum EngineCoreType {
     /**
-     * This engine only has a cheerio core
+     * This engine only has a cheerio core.
+     * This means there is NO manipulation that can be done at all.
+     * Pages come as they are and you cannot manipulate it. This is the fastest core hands down
      */
     Cheerio = 0,
+
     /**
-     * This engine only has a puppeteer core
+     * This engine only has a puppeteer core.
+     * With a puppeteer core you can do both reading and writing.
+     * But it requires more work. This is the slowest core, but the most powerful
      */
     Puppeteer = 1,
     /**
-     * This engine has both cheerio and puppeteer cores
+     * This engine has both cheerio and puppeteer cores.
+     * Puppeteer is used to manipulate the page.
+     * Cheerio is used to parse the page.
+     * This works well when you are trying to keep it simple, but is definitely not optimal
+     * Performance will vary based on engine implementation
      */
     CheerioAndPuppeteer = 2,
+
+    /**
+     * Heck if I know bud. Probably shouldn't be making it unknown in the first place
+     */
     Unknown = 99999999, // literally some arbitary number
 }
 
--- src/engines/web_scraping_engine.ts
+++ src/engines/web_scraping_engine.ts
@@ -1,9 +1,96 @@
-import EngineModes from './engine_mode';
+import EngineMode from './engine_mode';
 import EngineType from './engine_type';
 import EngineCoreType from './engine_core_type';
 
 export interface WebScrapingEngine {
-    getEngineMode(): EngineModes;
+    // any method that we want to relay information back should go here
+    getEngineMode(): EngineMode;
     getEngineType(): EngineType;
     getEngineCoreType(): EngineCoreType;
+
+    /**
+     * Decides what mode the engine should be in.
+     * The engine should only be in one mode at a time. Either Parsing or manipulating
+     * @param newMode The new mode that we want to lock the engine into
+     */
+    setEngineMode(newMode: EngineMode): boolean;
+
+    /**
+     * Start the engine up and get ready to process what comes in
+     */
+    startup(): Promise<void>;
+
+    /**
+     * Processes the url while the the engine is running and doing its thing
+     * @param url The url we want to target
+     */
+    process(url: string): Promise<void>;
+
+    /**
+     * Shut the engine off and free up any resources
+     */
+    shutoff(): Promise<void>;
+}
+
+/**
+ * Defines the methods that should be implemented in an engine that supports manipulation
+ */
+export interface ManipulationEngine<PageCore> {
+    /**
+     * Set the value into the select element
+     * @param querySelector The selector that targets the select element
+     * @param value The value to attempt to set
+     */
+    select(querySelector: string, value: string): Promise<void>;
+
+    /**
+     * Click on the element that matches the query selector
+     * @param querySelector The selector that targets the element we want to click
+     */
+    click(querySelector: string): Promise<void>;
+
+    /**
+     * Type a specific value into the field that can accept typing.
+     * @param querySelector The element(s) that we are targeting
+     * @param value The value we want to simulate typing in
+     */
+    type(querySelector: string, value: string): Promise<void>;
+
+    /**
+     * Gets the raw page core used for manipulating
+     */
+    getManipulationCore(): PageCore;
+}
+
+/**
+ * Defines the methods that should be implemented in an engine that supports parsing the page
+ */
+export interface ParsingEngine<PageCore> {
+    // getting text methods
+    getText(querySelector: string): Promise<string>;
+    getTextAll(querySelector: string): Promise<string[]>;
+
+    // attribute methods
+    getAttribute(querySelector: string, attributeName: string): Promise<string>;
+    getAttributeAll(querySelector: string, attributeName: string): Promise<string[]>;
+
+    // html methods
+    getHtml(querySelector: string): Promise<string>;
+    getHtmlAll(querySelector: string): Promise<string>;
+
+    /**
+     * Gets all the option elements that can be found in the select element mathing the query selector
+     * @param querySelector The query selector that targets the select element
+     */
+    getSelectOptions(
+        querySelector: string,
+    ): Promise<
+        {
+            key: string;
+            value: string;
+        }[]
+    >;
+
+    // gets the direct page core for more advanced usage
+    getParsingCore(): PageCore;
 }
