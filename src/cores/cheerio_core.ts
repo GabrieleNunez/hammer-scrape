253b12b893a9d238898330a687ec4cb74aa6a1e9
GabrieleNunez <g_m_nunez@live.com>
2019-07-28 23:28:49 -0700

    Added some more depth to the initialize method. Method now creates a cheerio request and runs it. Upon completion, the promise returned by the method is resolved

--- src/cores/cheerio_core.ts
+++ src/cores/cheerio_core.ts
@@ -2,21 +2,32 @@ import { CheerioRequest } from 'request-group-cheerio';
 import { ParsingCore } from '../web_scraping_engine';
 
 interface ExpectedLoadingFields {
-    url: string;
+    header?: string;
 }
 
 export class CheerioParsingCore extends ParsingCore<CheerioStatic, ExpectedLoadingFields> {
+    private request: CheerioRequest | null;
     public constructor(url: string) {
         super(url);
+        this.request = null;
     }
 
     public dispose(): Promise<void> {
-        throw new Error('Method not implemented.');
+        return new Promise((resolve): void => {
+            resolve();
+        });
     }
 
-    public initialize(data: ExpectedLoadingFields): Promise<void> {
-        throw new Error('Method not implemented.');
+    public initialize(data?: ExpectedLoadingFields): Promise<void> {
+        return new Promise((resolve): void => {
+            let userAgent: string | null = data && typeof data['header'] !== 'undefined' ? data.header : null;
+            this.request = new CheerioRequest(this.getUrl(), userAgent);
+            this.request.run().then((): void => {
+                resolve();
+            });
+        });
     }
+
     public getText(querySelector: string): Promise<string> {
         throw new Error('Method not implemented.');
     }
