e208e3a4f661d91f6b54bd557e7ae442666b0395
GabrieleNunez <gabrielenunez@protonmail.com>
2019-08-05 20:46:01 -0700

    Adjusted engine modes in built in cheerio engine

--- src/engines/cheerio_engine.ts
+++ src/engines/cheerio_engine.ts
@@ -3,6 +3,7 @@ import EngineType from '../engine_type';
 import EngineCoreType from '../engine_core_type';
 import CheerioParsingCore from '../cores/cheerio_parsing';
 import EngineMode from 'engine_mode';
+import { EngineCannotSwitchModeError } from 'engine_errors';
 
 export class CheerioEngine extends WebScrapingEngine<CheerioParsingCore, null> {
     public constructor() {
@@ -20,19 +21,23 @@ export class CheerioEngine extends WebScrapingEngine<CheerioParsingCore, null> {
     }
 
     public process(url: string): Promise<void> {
-        return new Promise(
-            async (resolve): Promise<void> => {
-                this.setEngineMode(EngineMode.Loading);
-                let cheerioCore: CheerioParsingCore = new CheerioParsingCore(url);
-                cheerioCore.initialize({
-                    xml: false,
-                    header: undefined,
-                });
-                this.parsingCore = cheerioCore;
-                this.setEngineMode(EngineMode.Idling);
-                resolve();
-            },
-        );
+        if (this.getEngineMode() === EngineMode.Idling) {
+            return new Promise(
+                async (resolve): Promise<void> => {
+                    this.setEngineMode(EngineMode.Loading);
+                    let cheerioCore: CheerioParsingCore = new CheerioParsingCore(url);
+                    cheerioCore.initialize({
+                        xml: false,
+                        header: undefined,
+                    });
+                    this.parsingCore = cheerioCore;
+                    this.setEngineMode(EngineMode.Idling);
+                    resolve();
+                },
+            );
+        } else {
+            throw new EngineCannotSwitchModeError();
+        }
     }
 
     public shutoff(): Promise<void> {
