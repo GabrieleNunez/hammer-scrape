61e2a81f25eb3d2c67e1de3b8336db5bbd0adf8b
GabrieleNunez <gabrielenunez@protonmail.com>
2019-08-06 17:55:39 -0700

    Skeleton implemented for puppeteer engine

--- src/engines/puppeteer_engine.ts
+++ src/engines/puppeteer_engine.ts
@@ -5,3 +5,15 @@ import PuppeteerParsingCore from '../cores/puppeteer_parsing';
 import PuppeteerManipulatingCore from '../cores/puppeteer_parsing';
 import EngineMode from '../engine_mode';
 import { EngineCannotSwitchModeError } from '../engine_errors';
+
+export class PuppeteerEngine extends WebScrapingEngine<PuppeteerParsingCore, PuppeteerManipulatingCore> {
+    protected load(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    public process(url: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    public shutoff(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+}
