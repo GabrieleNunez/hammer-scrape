3312ffbc64c69a0284889231eba7143fc6e2f775
GabrieleNunez <gabrielenunez@protonmail.com>
2019-08-23 15:03:28 -0700

    Added all exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 // our entry point into this library
-
-export { HammerEngine } from './engines/hammer_engine';
+export * from './engines/hammer_engine';
+export * from './engines/cheerio_engine';
+export * from './engines/puppeteer_engine';
+export * from './cores/cheerio_parsing';
+export * from './cores/puppeteer_parsing';
+export * from './cores/puppeteer_manipulate';
+export * from './engine_core_type';
+export * from './engine_errors';
+export * from './engine_mode';
+export * from './engine_type';
+export * from './web_scraping_engine';
 
 // default export will be the hammer engine
 import HammerEngine from './engines/hammer_engine';
