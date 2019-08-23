408cde4542ad381786ae67e4583840e33579b6b8
GabrieleNunez <gabrielenunez@protonmail.com>
2019-08-22 18:12:04 -0700

    Updated references for PuppeteerEngine and did some testing

--- src/engines/puppeteer_engine.ts
+++ src/engines/puppeteer_engine.ts
@@ -2,7 +2,7 @@ import { WebScrapingEngine } from '../web_scraping_engine';
 import EngineType from '../engine_type';
 import EngineCoreType from '../engine_core_type';
 import PuppeteerParsingCore from '../cores/puppeteer_parsing';
-import PuppeteerManipulatingCore from '../cores/puppeteer_parsing';
+import PuppeteerManipulatingCore from '../cores/puppeteer_manipulate';
 import EngineMode from '../engine_mode';
 import { EngineCannotSwitchModeError } from '../engine_errors';
 
