111bd7cf90d3316008ecb8cc05a226835a99cb25
GabrieleNunez <gabrielenunez@protonmail.com>
2019-08-22 21:03:10 -0700

    Updated packages and included basic default export for index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 // our entry point into this library
+
+export { HammerEngine } from './engines/hammer_engine';
+
+// default export will be the hammer engine
+import HammerEngine from './engines/hammer_engine';
+export default HammerEngine;
