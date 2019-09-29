76b6c5d41d960c7757e5572bbbefeede0570af89
GabrieleNunez <gabrielenunez@protonmail.com>
2019-09-28 17:51:18 -0700

    Bump version number, and made a quick and dirty hammer request for request-group

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,34 @@ export * from './web_scraping_engine';
 
 // default export will be the hammer engine
 import HammerEngine from './engines/hammer_engine';
+import { Request, BaseWebRequest } from 'request-group';
+
+/** Quick and Dirty hammer request
+ * @todo Find a a way to share puppeteer manager instances between multiple hammer request. Right now this is really inefficiant
+ */
+export class HammerRequest extends BaseWebRequest<HammerEngine> {
+    protected pingSelector: string;
+    public constructor(url: string, pingSelector: string) {
+        super(url);
+        this.pingSelector = pingSelector;
+    }
+
+    public async dispose(): Promise<void> {
+        if (this.pageData) {
+            await this.pageData.shutoff();
+            this.pageData = null;
+        }
+    }
+
+    public async run(): Promise<Request<HammerEngine>> {
+        if (!this.pageData) {
+            this.pageData = new HammerEngine(this.pingSelector);
+            await this.pageData.startup();
+        }
+        // process a url
+        await this.pageData.process(this.getUrl());
+        return this;
+    }
+}
+
 export default HammerEngine;
